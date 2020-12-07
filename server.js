0046ba738de079859d7cf77511b5d0044884a0f5
frederico-campello <fredfmc33@gmail.com>
2020-12-07 15:03:32 -0300

    Teste

--- server.js
+++ server.js
@@ -14,26 +14,24 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', async (socket) => {
   console.log('user connected');
 
+  // handle messages
   const messages = await chatModel.getMessages();
   socket.emit('loadMessages', messages);
 
-  socket.emit('defaultNickname', 'Tryber');
+  socket.on('message', async (data) => {
+    const { chatMessage, nickname } = data;
+    const result = await chatModel.saveMessage(chatMessage, nickname);
+    io.emit('serverResponse', result);
+  });
+  //
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
-  });
-
-  socket.on('message', async (data) => {
-    const { chatMessage, nickname } = data;
-
-    const result = await chatModel.saveMessage(chatMessage, nickname);
-
-    io.emit('serverResponse', result);
   });
 });
 
