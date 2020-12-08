e0659722fa7ab6708a4b1a0f4d363c079fddb711
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-12-08 18:11:31 -0300

    cc 05

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-let userId = 0;
-
 // https://pt.stackoverflow.com/questions/6526/como-formatar-data-no-javascript
 // https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
 app.get('/', (_req, res) => {
@@ -17,7 +15,7 @@ io.on('connection', (socket) => {
   socket.on('message', async (message) => {
     const newDate = new Date();
     const data = newDate.toISOString().substr(0, 10).split('-').reverse()
-    .join('-');
+      .join('-');
     const time = newDate.toLocaleString([], { hour12: true }).substr(11);
     const composeMessage = await `${data} ${time} - ${message.nickname}: ${message.chatMessage}`;
     io.emit('showMsg', composeMessage);
