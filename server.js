b90a14b9700e8256f3143bc0a7447cba1fbdd2a9
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-12-08 15:43:36 -0300

    cc 02

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
-const webChatModel = require("./model/webchatModel");
+const webChatModel = require('./model/webchatModel');
 
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const moment = require('moment');
 
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "public"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-app.use("/", (req, res) => {
-  res.render("index.html");
+app.use('/', (req, res) => {
+  res.render('index.html');
 });
 
-io.on("connection", (socket) => {
-  console.log("Socket connectado", socket.id);
-  webChatModel.getAll().then((messages) => socket.emit("historicMessages", messages));
+io.on('connection', (socket) => {
+  console.log('Socket connectado', socket.id);
+  webChatModel
+    .getAll()
+    .then((messages) => socket.emit('historicMessages', messages));
 
-  socket.on("message", async (data) => {    
+  socket.on('message', async (data) => {
     const date = moment(new Date()).format('DD-MM-yyyy hh:mm:ss A');
-    const dados = { date , ...data };
+    const dados = { date, ...data };
 
     await webChatModel.add(dados);
 
-    socket.broadcast.emit("message", `<strong>${dados.date} - ${dados.nickname}</strong>: ${dados.chatMessage}`);
+    socket.broadcast.emit(
+      'message',
+      `<strong>${dados.date} - ${dados.nickname}</strong>: ${dados.chatMessage}`
+    );
   });
 });
 
-server.listen(3000, () => console.log("Ouvindo na porta 3000"));
+server.listen(3000, () => console.log('Ouvindo na porta 3000'));
