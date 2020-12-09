71640285663a1888e492ee492f9518bf9e7c203e
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-12-09 18:09:47 -0300

    requsito bla bla

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const moment = require('moment');
+const {} = require('unique-names-generator');
 
 const app = express();
 
@@ -19,15 +20,35 @@ app.use('/', (req, res) => {
   res.render('index.html');
 });
 
+// const addCart = () => {
+//   if (!cart[name]) cart[name] = { ...product, quantidade: 1 };
+
+// };
+
+// const removeCart = () => {
+//   delete cart[name];
+// };
+
+const usersOnline = {};
+
 io.on('connection', (socket) => {
   console.log('Socket connectado', socket.id);
+  const { id } = socket;
+
   webChatModel
     .getAll()
     .then((messages) => socket.emit('historicMessages', messages));
 
+  socket.emit('userConnect', usersOnline);
+
+  socket.on('disconect', () => socket.emit('offline', usersOnline[socket.id]));
+
   socket.on('message', async (data) => {
     const date = moment(new Date()).format('DD-MM-yyyy hh:mm:ss A');
     const dados = { date, ...data };
+    const { nickname } = data;
+
+    if (usersOnline[id] != data.nickname) usersOnline[id] = nickname;
 
     await webChatModel.add(dados);
 
