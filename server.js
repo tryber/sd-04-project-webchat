375bb16dd5adcc9838f9f7fb72cb087daab520b5
deboracosilveira <deboracosilveira@gmail.com>
2020-12-09 15:25:16 -0300

    Req 3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const moment = require('moment');
+const chatModel = require('./models/chatModel');
 
 const app = express();
 const http = require('http').createServer(app);
@@ -10,18 +11,26 @@ app.use('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('Conectado');
 
-  socket.on('message', (message) => {
+  const allMessages = await chatModel.getMessages();
+  allMessages.forEach((message) => {
+    const msg = `${message.created_on} - ${message.nickname}: ${message.chatMessage}`;
+    socket.emit('oldMessages', msg);
+  });
+
+  socket.on('message', async (message) => {
     const time = new Date();
     const timestamp = moment(time).format('DD-MM-yyyy HH:mm:ss');
+    await chatModel.writeNewMessage(message.chatMessage, message.nickname, timestamp);
     const msg = `${timestamp} - ${message.nickname}: ${message.chatMessage}`;
     io.emit('message', msg);
   });
 
   socket.on('disconnect', () => {
-    io.emit('adeus', { message: 'Poxa, fica mais, vai ter bolo :)' });
+    console.log('Desconectado')
+    io.emit('disconnect', 'Tchau');
   });
 });
 
