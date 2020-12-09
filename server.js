1c108654e8fc4915df120cbf0d214b5b942e3645
Filipe de Castro <filipeyoga@gmail.com>
2020-12-09 16:53:29 -0300

    Refactor: Linter

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const cors = require("cors");
-const path = require("path");
-const messageModels = require("./models/messageModel");
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const cors = require('cors');
+const path = require('path');
+const messageModels = require('./models/messageModel');
 
 const loggedUsers = {};
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
 // });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   loggedUsers[socket.id] = socket.id;
-  io.emit("loggedUsers", loggedUsers);
+  io.emit('loggedUsers', loggedUsers);
 
-  socket.on("send all messages", async () => {
+  socket.on('send all messages', async () => {
     const allMessages = await messageModels.getAllMessages();
-    io.emit("allMessages", allMessages);
+    io.emit('allMessages', allMessages);
   });
 
-  socket.on("message", async (msg) => {
+  socket.on('message', async (msg) => {
     const result = await messageModels.storeMessage(
       msg.nickname,
-      msg.chatMessage
+      msg.chatMessage,
     );
-    io.emit("message", result);
+    io.emit('message', result);
   });
 
-  socket.on("nicknameChange", (nicknameChangePayload) => {
+  socket.on('nicknameChange', (nicknameChangePayload) => {
     const { socketId, nickname } = nicknameChangePayload;
     loggedUsers[socketId] = nickname;
-    io.emit("loggedUsers", loggedUsers);
+    io.emit('loggedUsers', loggedUsers);
   });
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     delete loggedUsers[socket.id];
-    io.emit("loggedUsers", loggedUsers);
+    io.emit('loggedUsers', loggedUsers);
   });
 });
 
 http.listen(3000, () => {
-  console.log("listening on 3000");
+  console.log('listening on 3000');
 });
