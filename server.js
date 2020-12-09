11c47d8b485926722b9f2e94cf0f0d3c63a53907
juniomelos <juniomelos@gmail.com>
2020-12-09 15:39:13 -0300

    ok

--- server.js
+++ server.js
@@ -22,22 +22,33 @@ app.use('/', express.static(PUBLIC_PATH));
 io.on('connection', async (socket) => {
   socket.on('message', async ({ chatMessage, nickname }) => {
     const date = new Date();
-    const timestamp = moment(date).format('DD-MM-yyyy h:mm:ss A');
+    const timestamp = moment(date).format('DD-MM-yyyy HH:mm:ss');
     const sendMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
 
     io.emit('online', nickname);
     io.emit('message', sendMessage);
-    socket.broadcast.emit('message', sendMessage);
+    // socket.broadcast.emit('message', sendMessage);
     await saveMessages({ nickname, chatMessage, timestamp });
   });
 
   const messageDB = await allMessages();
+
+  // messageDB.forEach(({ chatMessage, nickname, timestamp }) => {
+  //   socket.emit('historico', `${timestamp} - ${nickname}: ${chatMessage}`);
+  // });
+
   const msgSend = [];
   messageDB.map((element) => {
     const { chatMessage, nickname, timestamp } = element;
     return msgSend.push(`${timestamp} - ${nickname}: ${chatMessage}`);
   });
   socket.emit('historico', msgSend);
+
+  // socket.on('nameChange', (nickname) => {
+  //   const index = msgSend.findIndex((e) => e.id === socket.id);
+  //   if (msgSend[index]) msgSend[index].nickname = nickname;
+  //   io.emit('userList', msgSend);
+  // });
 });
 
 server.listen(PORT, () => {
