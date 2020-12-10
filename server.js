f2533e70bde597741ca964d8255436bf473cff20
Tereza Cristina Carvalho <terezacristina@MacBook-Pro-de-Tereza.local>
2020-12-10 17:36:06 -0300

    req 1

--- server.js
+++ server.js
@@ -2,18 +2,51 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const moment = require('moment');
 
 const app = express();
 
 const socketIoServer = http.createServer(app);
 
 const io = require('socket.io')(socketIoServer);
+const models = require('./models/modelsMassage');
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
-io.on('connection', (socket) => {
-  console.log('Cliente conectado', socket.id);
+// sempre que o socketio receber uma conexão vai realizar o broadcast dela
+io.on('connection', async (socket) => {
+  const historyMessage = await models.getAllMessages();
+  historyMessage.forEach(({ chatMessage, nickname, timestamp }) => {
+    const historyMsg = `${timestamp} - ${nickname}: ${chatMessage}`;
+    socket.emit('Historico', historyMsg);
+  });
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const time = new Date();
+    const timestamp = moment(time).format('DD-MM-yyyy HH:mm:ss');
+
+    const renderMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
+
+    io.emit('message', renderMessage);
+    io.emit('online', nickname);
+
+    await models.createMessage(chatMessage, nickname, timestamp);
+  });
+
+  socket.on('changeName', async ({ nickname }) => {
+    io.emit('changeName', nickname);
+  });
 });
+
+// qnd servidor recebe uma connection esse evento é executado
+// io.on('connection', (socket) => {
+//   console.log('Cliente conectado', socket.id);
+//   socket.on('msg', (msg) => {
+//     console.log(msg);
+//     socket.broadcast.emit('msg', msg);
+
+//   });
+// });
 
 socketIoServer.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');
