146b1874178773a863c163150debe7ca974c48f5
ribaslucas <lucas@mundoribas.com.br>
2020-12-20 13:02:12 -0300

    Trocando a lógica do server

--- server.js
+++ server.js
@@ -1,53 +1,92 @@
+require('dotenv').config();
+
 const express = require('express');
-const http = require('http');
 const path = require('path');
+
 const moment = require('moment');
 
 const app = express();
+const http = require('http');
 
 const socketIoServer = http.createServer(app);
 
 const io = require('socket.io')(socketIoServer);
-const models = require('./model/modelMessages');
+const modelMessages = require('./model/modelMessages');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
+let onlineUsers = [];
+
 io.on('connection', async (socket) => {
-  const onlineUsers = [];
+  const history = await modelMessages.getAllMessages();
 
-  const historyMessage = await models.getAllMessages();
-  const msgHisto = [];
-  historyMessage.map((msg) => {
-    const { chatMessage, nickname, timestamp } = msg;
-    return msgHisto.push(`${timestamp} - ${nickname}: ${chatMessage}`);
-  });
-  io.emit('Historico', msgHisto);
-
-  socket.on('message', async ({ chatMessage, nickname }) => {
-    const time = new Date();
-    const timestamp = moment(time).format('DD-MM-yyyy HH:mm:ss');
-    const renderMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
-    io.emit('message', renderMessage);
-
-    await models.createMessage(chatMessage, nickname, timestamp);
+  history.forEach(({ chatMessage, nickname, timestamp }) => {
+    const previousMessages = `${timestamp} ${nickname} ${chatMessage}`;
+    socket.emit('history', previousMessages);
   });
 
-  socket.on('changeName', async ({ nickname }) => {
-    io.emit('changeName', nickname);
+  socket.on('message', async ({ nickname, chatMessage }) => {
+    const formatedTimestamp = moment().format('DD-MM-yyyy HH:mm:ss');
+
+    const message = `(${formatedTimestamp}) - ${nickname} says: ${chatMessage}`;
+    io.emit('message', message);
+    return modelMessages.createMessage(chatMessage, nickname, formatedTimestamp);
   });
 
-  socket.on('changeName', async ({ nickname }) => {
-    onlineUsers.push(nickname);
-    io.emit('online', onlineUsers);
+  socket.on('usersOnline', ({ nickname }) => {
+    onlineUsers.push({ socketId: socket.id, nickname });
+    io.emit('usersOnlineUpdate', onlineUsers);
+  });
+
+  socket.on('updateNickname', ({ newName }) => {
+    console.log(onlineUsers);
+    onlineUsers = onlineUsers.filter(({ nickname }) => nickname !== newName);
+    console.log(onlineUsers);
+    onlineUsers.push({ socketId: socket.id, nickname: newName });
+    io.emit('usersOnlineUpdate', onlineUsers);
   });
 
   socket.on('disconnect', () => {
-    delete onlineUsers[socket.id];
-    io.emit('online', onlineUsers);
+    onlineUsers = onlineUsers.filter(({ socketId }) => socketId !== socket.id);
+    io.emit('usersOnlineUpdate', onlineUsers);
   });
 });
+
+// io.on('connection', async (socket) => {
+//   const historyMessage = await modelMessages.getAllMessages();
+//   const msgHisto = [];
+//   historyMessage.map((msg) => {
+//     const { chatMessage, nickname, timestamp } = msg;
+//     return msgHisto.push(`${timestamp} - ${nickname}: ${chatMessage}`);
+//   });
+
+//   io.emit('history', msgHisto);
+
+//   socket.on('message', async ({ chatMessage, nickname }) => {
+//     const time = new Date();
+//     const timestamp = moment(time).format('DD-MM-yyyy HH:mm:ss');
+//     const renderMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
+//     io.emit('message', renderMessage);
+
+//     await modelMessages.createMessage(chatMessage, nickname, timestamp);
+//   });
+
+//   socket.on('changeName', async ({ nickname }) => {
+//     io.emit('changeName', nickname);
+//   });
+
+//   socket.on('changeName', async ({ nickname }) => {
+//     onlineUsers.push(nickname);
+//     io.emit('online', onlineUsers);
+//   });
+
+//   socket.on('disconnect', () => {
+//     delete onlineUsers[socket.id];
+//     io.emit('online', onlineUsers);
+//   });
+// });
 
 const PORT = process.env.PORT || 3000;
 
