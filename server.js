4626d09b862870acf444d41146df2e2bdf72b000
wistyk <leonardoxw@gmail.com>
2021-01-01 22:31:34 -0300

    esqueleto baseado na aula da rocketseat montado e operando

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+// const cors = require('cors');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'public'));
+// app.engine('html', require('ejs').renderFile);
+
+// app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+  res.render('index.html');
+});
+
+const messages = [];
+
+io.on('connection', (socket) => {
+  console.log(`Socket conectado: ${socket.id}`);
+
+  socket.emit('previewsMessages', messages);
+
+  socket.on('message', (data) => {
+    console.log(data)
+    messages.push(data);
+    socket.broadcast.emit('receivedMessage', data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('conectado');
+});
