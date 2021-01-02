343aeffb10aabb174acfe9b188c1d0b7f1b6762f
wistyk <leonardoxw@gmail.com>
2021-01-01 22:38:00 -0300

    aplicado as configuracoes da aula do vini. Tudo funcionando sem erros

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const path = require('path');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.set('views', path.join(__dirname, 'public'));
-// app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
 
-// app.set('view engine', 'html');
+app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
   res.render('index.html');
