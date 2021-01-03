d4f6a119322cecd5286a0f39dbab080179dfd405
danilossousa <danilosantossousa@gmail.com>
2021-01-03 16:45:15 -0300

    v1.0.1

---
+++ models/messageModel.js
@@ -0,0 +1,19 @@
+const connect = require('./connection');
+
+const saveMessage = async (chatMessage, nickname, timestamp) =>
+  connect()
+    .then((db) =>
+      db.collection('messages').insertOne({ chatMessage, nickname, timestamp }))
+    .then(({ insertedId }) => ({
+      _id: insertedId,
+      chatMessage,
+      nickname,
+      timestamp,
+    }))
+    .catch((error) => error);
+
+const getAllMessage = async () =>
+  connect()
+    .then((db) => db.collection('messages').find({}).toArray());
+
+module.exports = { saveMessage, getAllMessage };
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const moment = require('moment');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+
+const { getAllMessage, saveMessage } = require('./models/messageModel');
+
+const PORT = 3000;
+let usersOnline = [];
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.use('/', (_req, res) => {
+  res.sendFile(`${__dirname}/client/index.html`);
+});
+
+io.on('connection', async (socket) => {
+  const previousMessage = await getAllMessage();
+
+  previousMessage.forEach(({ chatMessage, timestamp, nickname }) => {
+    const messageToSend = `${nickname} ${timestamp} ${chatMessage}`;
+    socket.emit('history', messageToSend);
+  });
+
+  socket.on('message', async ({ nickname, chatMessage }) => {
+    const time = new Date();
+    const timeEdited = moment(time).format('D-M-yyyy hh:mm:ss');
+    const renderMessages = `${nickname} ${timeEdited} ${chatMessage}`;
+
+    io.emit('message', renderMessages);
+
+    return saveMessage(chatMessage, nickname, timeEdited);
+  });
+
+  socket.on('user-login', ({ nickname }) => {
+    usersOnline.push({ socketId: socket.id, nickname });
+    io.emit('update-users', usersOnline);
+  });
+
+  socket.on('name-change', ({ newNickname }) => {
+    usersOnline = usersOnline.filter(({ nickname }) => nickname !== newNickname);
+    usersOnline.push({ socketId: socket.id, nickname: newNickname });
+    io.emit('update-users', usersOnline);
+  });
+
+  socket.on('disconnect', () => {
+    usersOnline = usersOnline.filter(({ socketId }) => socketId !== socket.id);
+    io.emit('update-users', usersOnline);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Running on port ${PORT}...`);
+});
