ec5911a3b5c7bf3c2e63ee8e522857be328b607c
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-03 17:45:56 -0300

    Req. 3 - histórico do chat

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const cors = require('cors');
 const path = require('path');
 const socketIo = require('socket.io');
 
+const messagesModel = require('./models/messagesModel');
+
 const app = express();
 const socketIoServer = http.createServer(app);
 const io = socketIo(socketIoServer, {
@@ -28,26 +30,61 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-io.on('connect', (socket) => {
+// Para ficar mais fácil de ler o código,
+// pense no 'socket' como um 'client'.
+io.on('connect', async (socket) => {
   console.log('Conectado');
+
+  // histórico de mensagens
+  const previousMessage = await messagesModel.getAll();
+  console.log('PREVIOUSMESSAGE: ', previousMessage);
+  io.emit('previousMessage', previousMessage);
+
+  // Random nickname
+  // const randomNick = `Guest_${socket.id}`;
+  const randomNick = ''; 
+  console.log('RandomNick: ',randomNick);
+  io.emit('join', randomNick, socket.id);
+  io.emit('listNicknameServer', randomNick, socket.id);
 
   socket.on('disconnect', () => {
     console.log('Desconectado');
-    io.emit('adeus', { mensagem: 'Poxa, fica mais, vai ter bolo :)' });
+    console.log('Desconectado: ', socket.id);
+    io.emit('exit', socket.id);
   });
 
-  socket.on('message', (message) => {
+  // socket.on('previousMessage', (previousMessage) => {});
+
+  socket.on('message', async (message) => {
     const { chatMessage, nickname } = message;
+    const timestamp = new Date();
+    // DD-MM-yyyy
+    const dateMessage = `${timestamp.getDate()}-${timestamp.getMonth() + 1}-${timestamp.getFullYear()}`;
+    // HH:mm:ss
+    const timeMessage = `${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}`;
+    // DD-MM-yyyy HH:mm:ss - nickname: chatMessage
+    const formatedMessage = `${dateMessage} ${timeMessage}-${nickname}: ${chatMessage}`;
+
     console.log(`Mensagem ${message}`);
     console.log('MESSAGE: ', nickname, chatMessage);
-    io.emit('messageServer', message);
+    console.log('FORMATEDMESSAGE: ', formatedMessage);
+
+    // Adiciona a mensagem no banco de dados.
+    const add = await messagesModel.add(nickname, formatedMessage, timeMessage);
+    console.log('ADD: ', add);
+    io.emit('messageServer', formatedMessage);
   });
 
-  socket.on('nicknameSave', (nickname) => {
-    io.emit('nicknameSaveServer', nickname);
+  socket.on('changeNickname', (nickname) => {
+    io.emit('listNicknameServer', nickname, socket.id);
   });
-  socket.broadcast.emit('nicknameSaveServer');
+
+  socket.broadcast.emit('listNicknameServer');
   socket.broadcast.emit('messageServer');
+
+  // console.log('----- SOCKET -----');
+  // console.log(socket);
+  // console.log('----- // -----');
 });
 
 socketIoServer.listen(3000, () => {
