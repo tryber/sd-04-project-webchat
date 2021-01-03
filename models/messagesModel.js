0fdd6a217dac1655477ffafc3f5e7bffc0f347c0
Giorgia <giorgialeoniac@gmail.com>
2021-01-03 17:40:52 -0300

    Começando o HTML, model iniciado

---
+++ models/messagesModel.js
@@ -0,0 +1,22 @@
+const connection = require('./connection');
+
+const getAllMessages = async () =>
+  connection()
+    .then((db) => db.collection('messages').find().toArray())
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+const sendMessage = async (chatMessage, nickname, timestamp) => connection()
+  .then((db) => db.collection('messages').insertOne({ chatMessage, nickname, timestamp }))
+  .then(() => ({
+    chatMessage,
+    nickname,
+    timestamp,
+  }));
+
+module.exports = {
+  getAllMessages,
+  sendMessage,
+};
