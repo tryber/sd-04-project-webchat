14ed2e762a06d42754add700aa8a378451ce62f4
caiolmf <caio.lmf@gmail.com>
2021-01-04 12:30:41 -0300

    finish all features

--- server.js
+++ server.js
@@ -2,19 +2,12 @@ require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
 const express = require('express');
-const moment = require('moment');
+
+const PUBLIC_PATH = path.join(__dirname, 'public');
+const PORT = process.env.PORT || 3000;
 
 /** Express app */
 const app = express();
-
-/** Web Server */
-const server = require('http').createServer(app);
-
-const PORT = process.env.PORT || 3000;
-const PUBLIC_PATH = path.join(__dirname, 'public');
-
-/** Websocket */
-const io = require('socket.io')(server);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,37 +15,14 @@ app.use(cors());
 
 app.use('/', express.static(PUBLIC_PATH));
 
-/* io.on('connection', async (socket) => {
-  socket.on('message', async ({ chatMessage, nickname }) => {
-    const date = new Date();
-    const timestamp = moment(date).format('DD-MM-yyyy HH:mm:ss');
-    const sendMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
+/** Web Server */
+const server = require('http').createServer(app);
 
-    io.emit('online', nickname);
-    io.emit('message', sendMessage);
-    // socket.broadcast.emit('message', sendMessage);
-    await saveMessages({ nickname, chatMessage, timestamp });
-  });
-
-  const messageDB = await allMessages();
-
-  // messageDB.forEach(({ chatMessage, nickname, timestamp }) => {
-  //   socket.emit('historico', `${timestamp} - ${nickname}: ${chatMessage}`);
-  // });
-
-  const msgSend = [];
-  messageDB.map((element) => {
-    const { chatMessage, nickname, timestamp } = element;
-    return msgSend.push(`${timestamp} - ${nickname}: ${chatMessage}`);
-  });
-  socket.emit('historico', msgSend);
-
-  // socket.on('nameChange', (nickname) => {
-  //   const index = msgSend.findIndex((e) => e.id === socket.id);
-  //   if (msgSend[index]) msgSend[index].nickname = nickname;
-  //   io.emit('userList', msgSend);
-  // });
-}); */
+/** Websocket */
+const socketIo = require('socket.io')(server);
+const socketService = require('./services/messagesSocket');
+/** Initialize socket */
+socketService(socketIo);
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
