b5edb576a223947197913343977d1357439b0ab1
lumaarrud <luma.oarruda@gmail.com>
2021-01-04 11:59:19 -0300

    transferindo projeto pra mac e testando

---
+++ models/chatModel.js
@@ -0,0 +1,18 @@
+
+const connection = require('./connection');
+
+const getMessages = async () => {
+  const messages = await connection().then((db) => db.collection('messages').find({}).toArray());
+  return messages;
+};
+
+const setNewMessage = async (chatMessage, nickname, timestamp) => {
+  const message = await connection().then((db) =>
+    db.collection('messages').insertOne({ chatMessage, nickname, created_on: timestamp }));
+  return message.ops[0];
+};
+
+module.exports = {
+  getMessages,
+  setNewMessage,
+};
