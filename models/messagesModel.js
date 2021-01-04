cd5b029c1b79438626465320f6c634cc72fd8d04
Giorgia <giorgialeoniac@gmail.com>
2021-01-04 10:05:41 -0300

    testando

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -1,16 +1,16 @@
 const connection = require('./connection');
 
-const getAllMessages = async () =>
-  connection()
-    .then((db) => db.collection('messages').find().toArray())
-    .catch((err) => {
-      console.log(err);
-      throw err;
-    });
+const getAllMessages = async () => connection()
+  .then((db) => db.collection('messages').find({}).toArray())
+  .catch((err) => {
+    console.log(err);
+    throw err;
+  });
 
 const sendMessage = async (chatMessage, nickname, timestamp) => connection()
   .then((db) => db.collection('messages').insertOne({ chatMessage, nickname, timestamp }))
-  .then(() => ({
+  .then(({ insertedId }) => ({
+    _id: insertedId,
     chatMessage,
     nickname,
     timestamp,
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+const http = require('http');
+const moment = require('moment');
+
+const socketServer = http.createServer(app);
+const io = require('socket.io')(socketServer);
+
+const users = [];
+const msgHistory = [];
+const models = require('./models/messagesModel');
+
+app.use(express.json());
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+io.on('connection', async (socket) => {
+  const callHistory = await models.getAllMessages();
+
+  callHistory.map((msg) => {
+    const { chatMessage, nickname, timestamp } = msg;
+    return msgHistory.push(`${timestamp} - ${nickname}: ${chatMessage}`);
+  });
+  io.emit('histórico', msgHistory);
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const newDate = new Date();
+    const timestamp = moment(newDate).format('DD-MM-yyyy HH:mm:ss');
+    const newMsg = `${timestamp} - ${nickname}: ${chatMessage}`;
+    io.emit('message', newMsg);
+
+    await models.sendMessage(chatMessage, nickname, timestamp);
+  });
+
+  socket.on('newNickName', async ({ nickname }) => {
+    io.emit('newNickName', nickname);
+  });
+
+  socket.on('usersOnline', async ({ nickname }) => {
+    users.push(nickname);
+    io.emit('online', nickname);
+  });
+
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('online', users);
+  });
+});
+
+socketServer.listen(3000, () => console.log('Ouvindo na porta 3000'));
