43745b7304ce79e52321f75e47bca32426f3b35d
caiolmf <caio.lmf@gmail.com>
2021-01-04 09:08:28 -0300

    add index public page

--- server.js
+++ server.js
@@ -15,14 +15,44 @@ const PUBLIC_PATH = path.join(__dirname, 'public');
 
 /** Websocket */
 const io = require('socket.io')(server);
-const { saveMessages, allMessages } = require('./models/messagesModel');
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 app.use('/', express.static(PUBLIC_PATH));
+
+/* io.on('connection', async (socket) => {
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const date = new Date();
+    const timestamp = moment(date).format('DD-MM-yyyy HH:mm:ss');
+    const sendMessage = `${timestamp} - ${nickname}: ${chatMessage}`;
+
+    io.emit('online', nickname);
+    io.emit('message', sendMessage);
+    // socket.broadcast.emit('message', sendMessage);
+    await saveMessages({ nickname, chatMessage, timestamp });
+  });
+
+  const messageDB = await allMessages();
+
+  // messageDB.forEach(({ chatMessage, nickname, timestamp }) => {
+  //   socket.emit('historico', `${timestamp} - ${nickname}: ${chatMessage}`);
+  // });
+
+  const msgSend = [];
+  messageDB.map((element) => {
+    const { chatMessage, nickname, timestamp } = element;
+    return msgSend.push(`${timestamp} - ${nickname}: ${chatMessage}`);
+  });
+  socket.emit('historico', msgSend);
+
+  // socket.on('nameChange', (nickname) => {
+  //   const index = msgSend.findIndex((e) => e.id === socket.id);
+  //   if (msgSend[index]) msgSend[index].nickname = nickname;
+  //   io.emit('userList', msgSend);
+  // });
+}); */
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
