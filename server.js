37e9eb5473d0d0649846910a1d4d7c47300cd00a
pedrotpo <pedrotpoliveira@gmail.com>
2021-01-11 15:51:13 -0300

    Intial Settings

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+server.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
