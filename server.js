b87f867a6f8c2c4476b258b2f8d210d4d9828f68
leandromarten1 <leandromarten1@gmail.com>
2021-01-14 19:00:35 -0300

    aparentemente funciona os users e mesg

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
 const moment = require('moment');
+const Model = require('./models/chatModel');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,35 +17,40 @@ app.use(express.json());
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const formatMessage = (nickname, message, timestamp) => {
-	return `${timestamp} - ${nickname}: ${message}`
-}
-
 let users = [];
 
+const formatMessage = (nickname, message, timestamp) => {
+  return `${timestamp} - ${nickname}: ${message}`;
+};
+
 // Conexão
-io.on('connection', (socket) => {
-	const user = {
-		id: socket.id
-	};
-	// console.log('User Connected', user);
+io.on('connection', async (socket) => {  
+	// const user = {
+	// 	id: socket.id,
+	// 	nickname: 'Anonymous',
+	//   };
 
-	socket.emit('onlineUsers', [...users, {...user}])
-	users.push(user)
+	io.emit('onlineUsers', users)
+	
+ 	const history = await Model.getMessages();
+	socket.emit('history', history);  
+	  
+	socket.broadcast.emit('message', 'New user logged')
 
-	socket.on('message', ({chatMessage, nickname}) => {
-		nickname !== '' ? user.nickname = nickname : user.nickname = 'Anonymous';
-		const date = Date.now();
-		const time = moment(date).format('DD-MM-YYYY h:mm:ss a');
-		users.push(user)
-		socket.emit('onlineUsers', users)
+  	socket.on('message', async ({ chatMessage, nickname = 'Anonymous' }) => {
+   		const date = Date.now();
+   		const time = moment(date).format('DD-MM-YYYY h:mm:ss a');
 		io.emit('message', formatMessage(nickname, chatMessage, time));
-	})
+		users.push({id: socket.id, nickname})
+		io.emit('onlineUsers', users)
+    	// save in BD
+    	await Model.saveMessage(chatMessage, nickname, time);
+  	});
 
-	socket.on('disconnect', () => {
-		users = users.filter(user => socket.id !== user.id);
-		socket.emit('onlineUsers', users)
-	})
+  	socket.on('disconnect', () => {
+		  users = users.filter(user => socket.id !== user.id)
+		  io.emit('onlineUsers', users)
+	  });
 });
 
 server.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
