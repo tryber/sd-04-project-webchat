94cd50a98a9f659d4e9caf19dd7380dd1fc9e777
Ronald Lima <ronaldazevedolima@gmail.com>
2021-01-20 09:13:24 -0300

    cc2

--- models/message.js
+++ models/message.js
@@ -3,7 +3,7 @@ const connection = require('./connection');
 const addMessage = async (chatMessage, nickname, timestamp) => {
   const message = await connection().then((db) => db.collection('messages').insertOne({ chatMessage, nickname, timestamp }));
   return message;
-}; 
+};
 
 const getAllMessages = async () => {
   const allMessages = connection().then((db) => db.collection('messages').find({}).toArray());
