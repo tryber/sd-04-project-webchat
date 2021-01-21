4a543332d959b5c37ae022900d7a7104d48de5d4
djonata-dias <diasdjonata@gmail.com>
2021-01-21 16:58:39 -0300

    Ajuste de linter

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const http = require('http')
+const http = require('http');
 const cors = require('cors')();
 const socket_io = require('socket.io')
 const path = require('path');
-let guestId = 0
+
+let guestId = 0;
 
 const socketIoServer = http.createServer(app);
 const io = socket_io(socketIoServer,
   {
     cors: {
       origin: 'http://localhost:3000/',
-      methods: ['GET', 'POST']
-    }
+      methods: ['GET', 'POST'],
+    },
   });
 
 const sockets = [];
@@ -26,14 +28,15 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
   guestId++;
   console.log('Conectado');
-  sockets.push(socket)
-  socket.emit("Seja bem vindo")
+  socket.emit("Seja bem vindo");
   socket.broadcast.emit('mensagemServer');
-  socket.user = `Guest${guestId}`
+  const user = `Guest${guestId}`;
+  sockets.push(socket);
+  io.emit('listing',{user});
 
   socket.on('disconnect', () => {
     io.emit('adeus', { mensagem: 'Poxa, fica mais, vai ter bolo :)' });
-    sockets.splice(sockets.indexOf(socket), 1)
+    sockets.splice(sockets.indexOf(socket), 1);
     console.log('Adeus');
   });
 
