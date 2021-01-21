e636b435a311a4125f4859bf0ec4d5736d0af481
djonata-dias <diasdjonata@gmail.com>
2021-01-21 17:53:23 -0300

    faltou uma semicolon

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 let guestId = 0;
 
 const socketIoServer = http.createServer(app);
