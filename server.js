adfa429c5acfbaaed3c338d4162f86978728d8ed
jessica-tavares <jessicatavaraeseel@gmail.com>
2021-01-22 19:07:37 -0300

    teste 2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
 const path = require('path');
-const cors = require('cors');
 const moment = require('moment');
 const { getAllMsg, createMsg, getPrivateMessages, createPrivateMsg } = require('./models/msgModel');
 
@@ -12,81 +11,69 @@ const io = socketIo(socketIoServer); // a partir daqui que escuta
 
 const PUBLIC = path.join(__dirname, 'public');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.json());
-
 app.use(express.static(PUBLIC));
 
 app.get('/', (_req, res) => {
   res.sendFile('index.html'); // conecta com o index.html
 });
 
-let onlineUsers = [];
-let thisUser;
+const onlineUsers = {};
 
 io.on('connection', async (socket) => {
-  const allMsg = await getAllMsg();
-  const allPrivateMsg = await getPrivateMessages();
+  console.log(`${socket.id} conectado!`);
 
-  io.emit('onlineUsers', onlineUsers);
+  onlineUsers[socket.id] = socket.id;
 
-  socket.on('newUser', (nick) => {
-    console.log('esse é o nome atual', nick); // usuario
-    thisUser = { id: socket.id, nick };
-    onlineUsers.push(thisUser);
-    // console.log(onlineUsers);
-    io.emit('newUser', onlineUsers);
+  const history = await getAllMsg();
+
+  io.emit('history', history);
+
+  socket.on('newHistory', async () => {
+    const newHistory = await getAllMsg();
+    socket.emit('renderNewHistory', newHistory);
   });
 
-  const msgSend = [];
-  allMsg.map((element) => {
-    const { chatMessage, nickname, timestamp } = element;
-    return msgSend.push(`${timestamp} - ${nickname}: ${chatMessage}`);
+  const privateHistory = await getPrivateMessages();
+
+  io.emit('privateHistory', privateHistory);
+
+  socket.on('privateHistory', async (selectedUser) => {
+    const newPrivateHistory = await getPrivateMessages();
+    socket.emit('renderPrivateHistory', { newPrivateHistory, selectedUser });
   });
 
-  socket.emit('history', msgSend);
+  socket.emit('newUser', socket.id);
 
-  socket.on('message', async (message) => { // emite e salva mensagens
-    const time = moment(new Date()).format('DD-MM-YYYY h:mm:ss a');
-    await createMsg(message.chatMessage, message.nickname, time);
-    const fullMsgString = `${time} - ${message.nickname}: ${message.chatMessage}`;
-    io.emit('message', fullMsgString);
+  socket.on('chosenNick', (data) => {
+    onlineUsers[socket.id] = data;
+    io.emit('updateUsers', onlineUsers);
   });
 
-  socket.on('newNickName', (newUser) => {
-    onlineUsers.map((user) => {
-      const thisNewUser = user;
-      if (thisNewUser.id === thisUser.id) thisNewUser.nick = newUser;
-      return console.log(user);
-    });
-    io.emit('newNickName', onlineUsers);
+  io.emit('updateUsers', onlineUsers);
+
+  socket.on('message', async (data) => {
+    const dateTime = new Date().getTime();
+    const dateToUser = moment(dateTime).format('DD-MM-yyyy h:mm:ss A');
+    const message = `${dateToUser} - ${data.nickname}: ${data.chatMessage}`;
+    await createMsg(data.nickname, data.chatMessage, dateTime);
+    io.emit('message', message);
+  });
+
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+  socket.on('privateMessage', (data) => {
+    const dateTime = moment(new Date()).format('DD-MM-yyyy h:mm:ss A');
+    const message = `${dateTime} (private) - ${data.nickname}: ${data.chatMessage}`;
+    const sender = getKeyByValue(onlineUsers, data.nickname);
+    const receiver = getKeyByValue(onlineUsers, data.privateReceiver);
+    createPrivateMsg(data.nickname, data.chatMessage, dateTime);
+    io.to(sender).emit('new privateMessageSender', message);
+    io.to(receiver).emit('new privateMessage', message);
   });
 
   socket.on('disconnect', () => {
-    console.log('usuario se desconectou');
-    onlineUsers = onlineUsers.filter((user) => user.id !== socket.id);
-    io.emit('disconnect', onlineUsers);
+    delete onlineUsers[socket.id];
+    io.emit('updateUsers', onlineUsers);
   });
-
-  socket.on('privateMessage', async ({ anotherSocketId, msg, nickname }) => {
-    const time = moment(new Date()).format('DD-MM-YYYY h:mm:ss a');
-    const userTo = onlineUsers.find((user) => user.id === anotherSocketId);
-    await createPrivateMsg(msg, nickname, time, userTo.nick);
-    const privateMsgSend = `${nickname} - ${time}: ${msg}`;
-    io.to(socket.id).emit('privateMessage', privateMsgSend);
-    io.to(anotherSocketId).emit('privateMessageReceiver', privateMsgSend);
-  });
-
-  const allMsgPrivate = [];
-  allPrivateMsg.map((element) => {
-    const { chatMessage, nickname, timestamp } = element;
-    return allMsgPrivate.push(`${nickname} - ${timestamp}: ${chatMessage}`);
-  });
-
-  socket.emit('privateHistory', allMsgPrivate);
-});
-
-socketIoServer.listen(3000, () => {
-  console.log('Express ouvindo na porta 3000');
 });
