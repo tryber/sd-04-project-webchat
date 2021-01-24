24ddd08b516388ba6cf856ea8bda0910ece5d789
djonata-dias <diasdjonata@gmail.com>
2021-01-24 16:58:04 -0300

    adicionei o model nas funções para inserir as mensagens no banco

--- utils/formatMessage.js
+++ utils/formatMessage.js
@@ -1,9 +1,14 @@
+const { insertMessage } = require("../models/messageModel");
+
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth();
 const year = date.getFullYear();
 const time = new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(date);
 
-const formatMessage = (user, message) => `${day}-${month}-${year} ${time} ${user}: ${message}`;
+const formatMessage = async (user, message) => {
+  await insertMessage('messages', message)
+  return (`${day}-${month}-${year} ${time} ${user}: ${message}`)
+};
 
 module.exports = formatMessage;
