afed8a63462077278daedc3e820cef703eee51fc
djonata-dias <diasdjonata@gmail.com>
2021-01-24 16:56:24 -0300

    criei a model para inserir o historico de menssagens no banco de dados

---
+++ models/messageModel.js
@@ -0,0 +1,19 @@
+const connection = require('./connection');
+
+findAllMessages = async (collection) =>
+  connection()
+    .then((db) => db.collection(collection).findAll())
+    .catch((err) => {
+      console.error(err)
+      return process.exit()
+    })
+
+insertMessage = async (collection, message) =>
+  connection()
+    .then((db) => db.collection(collection).insertOne({ message }))
+    .catch((err) => {
+      console.error(err);
+      return process.exit(1)
+    })
+
+module.exports = { findAllMessages, insertMessage }
