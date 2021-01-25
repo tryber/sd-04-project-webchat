8ba32246150bac1418c7316353b39373fabeacc9
djonata-dias <diasdjonata@gmail.com>
2021-01-24 23:26:59 -0300

    ajuste de formatação

--- utils/formatMessage.js
+++ utils/formatMessage.js
@@ -1,12 +1,9 @@
-
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth();
 const year = date.getFullYear();
 const time = new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(date);
 
-const formatMessage = (user, message) => {
-  return `${day}-${month}-${year} ${time} ${user}: ${message}`;
-};
+const formatMessage = (user, message) => `${day}-${month}-${year} ${time} ${user}: ${message}`;
 
 module.exports = formatMessage;
