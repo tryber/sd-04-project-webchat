0c164cb6799ab1f4ddea1fb1ebfce5e121620d1c
djonata-dias <diasdjonata@gmail.com>
2021-01-25 17:16:32 -0300

    Ajuste na model que insere as mensagens no banco de dados

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,19 +1,25 @@
+const moment = require('moment');
 const connection = require('./connection');
 
 const findAllMessages = async () =>
   connection()
-    .then((db) => db.collection('messages').findAll())
+    .then((db) => db.collection('messages').find({}, { _id: 0 }).toArray())
     .catch((err) => {
       console.error(err);
       return process.exit();
     });
 
-const insertMessage = async (message) =>
+const insertMessage = async (chatMessage, nickname) => {
+  const messageDate = moment(new Date()).format('DD-MM-yyyy HH-mm:ss')
   connection()
-    .then((db) => db.collection('messages').insertOne({ message }))
+    .then((db) => {
+      db.collection('messages').insertOne({ chatMessage, nickname, messageDate })
+    })
     .catch((err) => {
       console.error(err);
       return process.exit(1);
     });
+  return `${messageDate} ${nickname}: ${chatMessage}`
+}
 
 module.exports = { findAllMessages, insertMessage };
