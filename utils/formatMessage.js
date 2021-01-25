54fb64073d098066ee1d22e306984f5d5c53c369
djonata-dias <diasdjonata@gmail.com>
2021-01-24 23:23:29 -0300

    tirei a inserção da mensagem no banco da função que formata a mensagem

--- utils/formatMessage.js
+++ utils/formatMessage.js
@@ -1,4 +1,3 @@
-const { insertMessage } = require('../models/messageModel');
 
 const date = new Date();
 const day = date.getDate();
@@ -6,9 +5,8 @@ const month = date.getMonth();
 const year = date.getFullYear();
 const time = new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(date);
 
-const formatMessage = async (user, message) => {
-  await insertMessage('messages', message);
-  return (`${day}-${month}-${year} ${time} ${user}: ${message}`);
+const formatMessage = (user, message) => {
+  return `${day}-${month}-${year} ${time} ${user}: ${message}`;
 };
 
 module.exports = formatMessage;
