3f029f5803d1326e85d94a681289d3edfd812438
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-25 16:46:57 -0300

    Create socketIoServer

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const socketIoServer = require('http').createServer();
+const io = require('socket.io')(socketIoServer);
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3000;
+const EXPRESS_PORT = process.env.EXPRESS_PORT ?? 3000;
+const SOCKET_IO_PORT = process.env.SOCKET_IO_PORT ?? 3333;
 
-app.listen(PORT, console.log(`Listening at ${PORT}`));
+app.use(express.json());
+
+app.listen(EXPRESS_PORT, console.log(`Express listening at port: ${EXPRESS_PORT}`));
+
+socketIoServer.listen(SOCKET_IO_PORT, console.log(`Socket.io listening at port: ${SOCKET_IO_PORT}`))
