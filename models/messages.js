756916ddc214ba2273850c401724462821d6ac86
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-01-26 20:38:32 -0300

    lint

--- models/messages.js
+++ models/messages.js
@@ -3,8 +3,7 @@ const connection = require('./connection');
 const create = async (chatMessage, nickname, timestamp) =>
   connection()
     .then((db) =>
-      db.collection('messages').insertOne({ chatMessage, nickname, timestamp })
-    )
+      db.collection('messages').insertOne({ chatMessage, nickname, timestamp }))
     .then(({ insertedId }) => ({
       chatMessage,
       nickname,
