7aae5098dd8e2370bc944cb412a849d88cf243fd
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-26 08:40:39 -0300

    Handle connection errors

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -1,32 +1,40 @@
 const connection = require('./connection');
 
 const createMessage = async (messageObj) => {
-  const conn = await connection();
-  const message = await conn.collection('messages').insertOne(messageObj);
-  return message.ops[0];
+  try {
+    const database = await connection();
+    const message = await database.collection('messages').insertOne(messageObj);
+    return message.ops[0];
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
 };
 
 const createPrivateMessage = async (messageObj) => {
-  const conn = await connection();
-  const message = await conn.collection('private').insertOne(messageObj);
-  return message.ops[0];
+  try {
+    const database = await connection();
+    const privateMessage = await database.collection('private').insertOne(messageObj);
+    return privateMessage.ops[0];
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
 };
 
 const getMessages = async () => {
-  const conn = await connection();
-  const messages = await conn.collection('messages').find({}).toArray();
-  return messages;
-};
-
-const getPrivateMessages = async () => {
-  const conn = await connection();
-  const messages = await conn.collection('private').find({}).toArray();
-  return messages;
+  try {
+    const database = await connection();
+    const messages = await database.collection('messages').find({}).toArray();
+    return messages;
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
 };
 
 module.exports = {
   createMessage,
   createPrivateMessage,
   getMessages,
-  getPrivateMessages,
 };
