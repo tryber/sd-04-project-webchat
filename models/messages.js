4c57a2966e761fcb15b7eef401ff3267275a2884
Caio Ribeiro <cacoribeiro@gmail.com>
2021-01-26 17:35:18 -0300

    Testando Bonus

--- models/messages.js
+++ models/messages.js
@@ -4,7 +4,8 @@ const save = ({ nickname, chatMessage }) =>
   connection().then((db) =>
     db
       .collection('messages')
-      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString() }));
+      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString() }),
+  );
 
 const getAll = async () => {
   const db = await connection();
@@ -15,7 +16,8 @@ const msgSendTo = ({ nickname, chatMessage, to }) =>
   connection().then((db) =>
     db
       .collection('private')
-      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString(), to }));
+      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString(), to }),
+  );
 
 const getPrivate = async (nickname, to) => {
   const db = await connection();
