aa6f01a37b616ad442c4476c3b4a78aaa1df74a0
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-01-26 01:19:32 -0300

    requisito 1

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+const moment = require('moment');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('Conectado');
+
+  socket.on('message', ({ nickname, chatMessage }) => {
+    const msg = {
+      nickname,
+      message: chatMessage,
+      timestamp: moment(new Date()).format('DD-MM-yyyy hh:mm:ss'),
+    };
+    io.emit('message', `${msg.timestamp} - ${nickname}: ${chatMessage}`, 'publick ');
+  });
+  socket.on('disconnect', () => {
+    console.log('Desconectado');
+  });
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
