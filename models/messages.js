37655198803c8c320e4aae5663631c20788efa83
Caio Ribeiro <cacoribeiro@gmail.com>
2021-01-26 17:39:18 -0300

    Passa so as vezes

--- models/messages.js
+++ models/messages.js
@@ -4,8 +4,7 @@ const save = ({ nickname, chatMessage }) =>
   connection().then((db) =>
     db
       .collection('messages')
-      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString() }),
-  );
+      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString() }));
 
 const getAll = async () => {
   const db = await connection();
@@ -16,8 +15,7 @@ const msgSendTo = ({ nickname, chatMessage, to }) =>
   connection().then((db) =>
     db
       .collection('private')
-      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString(), to }),
-  );
+      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString(), to }));
 
 const getPrivate = async (nickname, to) => {
   const db = await connection();
