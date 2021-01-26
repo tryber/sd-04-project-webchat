8166a01148798208345c79c63cfbb4b238acdf55
Caio Ribeiro <cacoribeiro@gmail.com>
2021-01-26 15:30:32 -0300

    Passa e nao passa

--- models/messages.js
+++ models/messages.js
@@ -4,14 +4,29 @@ const save = ({ nickname, chatMessage }) =>
   connection().then((db) =>
     db
       .collection('messages')
-      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString() }));
+      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString() }),
+  );
 
 const getAll = async () => {
   const db = await connection();
   return db.collection('messages').find({}).toArray();
 };
 
+const msgSendTo = ({ nickname, chatMessage, to }) =>
+  connection().then((db) =>
+    db
+      .collection('private')
+      .insertOne({ nickname, chatMessage, timestamp: new Date().toLocaleString(), to }),
+  );
+
+const getPrivate = async (nickname, to) => {
+  const db = await connection();
+  return db.collection('private').find({ nickname, to }).toArray();
+};
+
 module.exports = {
   save,
   getAll,
+  msgSendTo,
+  getPrivate,
 };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const express = require('express');
 
-const { save, getAll } = require('./models/messages');
+const { save, getAll, msgSendTo, getPrivate } = require('./models/messages');
 
 const app = express();
 
@@ -19,8 +19,10 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3000;
 
 let clients = [];
+let guestId = 0;
 
 io.on('connection', async (socket) => {
+  guestId++;
   // Traz todas mensagens do banco de dados e envia para o front
   const history = await getAll();
   io.emit('history', history);
@@ -33,11 +35,31 @@ io.on('connection', async (socket) => {
     io.emit('online-users', clients);
   });
 
+  socket.on('private-chat', async ({ privateStatus, nickname, to }) => {
+    console.log(privateStatus, nickname, to);
+    if (!privateStatus) {
+      const history = await getAll();
+      console.log(history);
+      io.emit('history', history);
+    }
+    const historyPrivate = await getPrivate(nickname, to);
+    console.log(historyPrivate);
+    return socket.emit('history', historyPrivate);
+  });
+
   // Escuta a mensagem vinda do FRONT e armazena no banco de dados,
   //  e retorna para o FRONT a msg formatada.
-  socket.on('message', async ({ chatMessage, nickname }) => {
+  socket.on('message', async ({ chatMessage, nickname, privateStatus, to }) => {
     const newDate = new Date();
     const date = moment(newDate).format('DD-MM-yyyy HH:mm:ss');
+    console.log(privateStatus);
+    if (privateStatus) {
+      const userTosend = clients.filter((client) => client.nickname === to)[0];
+      const msg = `${date} (private) - ${nickname}: ${chatMessage}`;
+      await msgSendTo({ nickname, chatMessage, date, to: userTosend.nickname });
+      io.to(socket.id).emit('message', msg);
+      return io.to(userTosend.id).emit('message', msg);
+    }
     const msg = `${date} - ${nickname}: ${chatMessage}`;
     io.emit('message', msg);
     const obj = {
@@ -49,6 +71,12 @@ io.on('connection', async (socket) => {
   });
 
   socket.on('logged', ({ nickname }) => {
+    if (nickname === 'Guest') {
+      const obj = { id: socket.id, nickname: `Guest${guestId}` };
+      clients.push(obj);
+      console.log('obj', obj);
+      return io.emit('online-users', clients);
+    }
     const obj = { id: socket.id, nickname };
     clients.push(obj);
     io.emit('online-users', clients);
