0855047ff0a0c3ed02db408a9f5d41a420cde619
matheusysd <matheusyurisd@gmail.com>
2021-01-27 00:54:30 -0300

    history messages req 3

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 const path = require('path');
 const moment = require('moment');
+const { emit } = require('process');
+
+const messagesModel = require('./models/messagesModel');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,8 +19,10 @@ app.use(cors());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('user connected');
+  const messages = await messagesModel.getMessages();
+  io.to(socket.id).emit('showMessageHistory', messages);
   const users = {};
   socket.on('userConection', (currentUser) => {
     users[socket.id] = currentUser;
@@ -27,13 +32,14 @@ io.on('connection', (socket) => {
     users[socket.id] = nickname;
     io.emit('displayUsers', users);
   });
-  socket.on('message', ({ nickname, chatMessage }) => {
+  socket.on('message', async ({ nickname, chatMessage }) => {
     const msg = {
       nickname,
       message: chatMessage,
       timestamp: moment(new Date()).format('DD-MM-yyyy hh:mm:ss'),
     };
-    io.emit('message', `${msg.timestamp} - ${nickname}: ${chatMessage}`);
+    const message = await messagesModel.newMessage(msg);
+    io.emit('message', `${message.timestamp} - ${message.nickname}: ${message.message}`);
   });
   socket.on('disconnect', () => {
     console.log('user disconnected');
